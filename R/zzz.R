09cf0068c525ddcab7cf8f50e8f3ba33d0ab113e
Al <ajalalabadi@unimelb.edu.au>
2019-05-14 20:17:14 +1000

    remove mixOmics version
    So that uninstalling mixOmics will not produce an error in package compilation

--- R/zzz.R
+++ R/zzz.R
@@ -24,11 +24,11 @@
 #############################################################################################################
 
 
-.onAttach <- function(libname, pkgname){ packageStartupMessage("\nLoaded mixOmics ",as.character(packageDescription("mixOmics")[["Version"]]),
-    
+.onAttach <- function(libname, pkgname){ packageStartupMessage("\nLoaded mixOmics2 ",as.character(packageDescription("mixOmics2")[["Version"]]),
+
     "\n\nThank you for using mixOmics! Learn how to apply our methods with our tutorials on www.mixOmics.org, vignette and bookdown on  https://github.com/mixOmicsTeam/mixOmics",
     "\nQuestions: email us at mixomics[at]math.univ-toulouse.fr  ",
     "\nBugs, Issues? https://github.com/mixOmicsTeam/mixOmics/issues",
     "\nCite us:  citation('mixOmics')"
-    
+
     )}
