9d4f229595f5658134c111f1af23308a5dc3f519
Al <ajalalabadi@unimelb.edu.au>
2019-05-21 23:56:03 +1000

    character column data added to corece to factor and then numeric

--- roxygenise.R
+++ roxygenise.R
@@ -11,7 +11,8 @@ mae_data <- miniACC
 X_index <- 1 ## RNASeq2GeneNorm
 Y_index_assay <- 2 ## gistict
 Y_index_coldata <- 2 ## "years_to_birth" - must be numeric
-Y_index_invalid_coldata <- 1 ## "years_to_birth" - must be numeric
+Y_index_invalid_coldata <- 1 ## "patientID" - must be numeric
+Y_index_char_coldata <- 7 ## pathologic_stage" - is in fact a valid class but not factor
 ## ----------------------------------------------------- no need to change the following
 
 X <- names(assays(mae_data))[X_index] ## X assay
@@ -30,6 +31,8 @@ Ycn <-  as.numeric(colData(mae_data[,,X])[,Yc]) ## Y column data numeric
 Y_inv <- colnames(colData(mae_data))[Y_index_invalid_coldata] ## invalid coldata y
 Y_inv.vec <- colData(mae_data)[,Y_index_invalid_coldata] ## vector
 
+Y_char <-colnames(colData(mae_data))[Y_index_char_coldata]
+Y_char.vec <- colData(mae_data)[,Y_index_char_coldata]
 
 devtools::test()
 ############# test examples
