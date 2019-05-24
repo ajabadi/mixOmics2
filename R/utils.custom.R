6fee7df6eb6b4b154b9b43ff1d23491ae681d575
Al <ajalalabadi@unimelb.edu.au>
2019-05-24 18:05:21 +1000

    custom condition handlers for better tests

---
+++ R/utils.custom.R
@@ -0,0 +1,75 @@
+## -----------------------------------------------------------------------------------
+## ---------------  custom stop to define specific error classes
+## -----------------------------------------------------------------------------------
+stop_custom <- function(.subclass, message, call = NULL, ...) {
+  formals(stop)$call. <- FALSE
+  err <- structure(
+    list(
+      message = message,
+      call = call,
+      ...
+    ),
+    class = c(.subclass, "error", "condition")
+  )
+  stop(err)
+}
+
+
+## ----------- for invalid signature
+.inv_signature <- function() stop_custom("inv_signature", "incorrect input format to 'pls'. See ?spls for supported signatures")
+## ----------- for invalid data
+.inv_mae <- function(data) stop_custom("inv_mae", paste0(squote(data), " must be a MultiAssayExperiment object"))
+## ----------- for invalid formula for single
+.inv_sformula <- function() stop_custom("inv_sformula", "'formula' must be a formula object of form Y~X where X and Y are numeric matrices")
+## ----------- for invalid formula for blocks
+.inv_bformula <- function() stop_custom("inv_bformula", "'formula' must be a formula object of form Y~X where X is a list of numeric matrices")
+## ----------- for invalid X/Y
+.inv_assay <- function() stop_custom("inv_assay", "invalid assay/colData name(s).")
+
+## -----------------------------------------------------------------------------------
+## ---------------  custom warnings with specified class
+## -----------------------------------------------------------------------------------
+warning_custom <- function(.subclass, message, call=NULL, ...){
+  formals(warning)$call. <- FALSE
+  warn <- structure(
+    list(
+      message = message,
+      call = call,
+      ...
+    ),
+    class = c(.subclass, "warning", "condition")
+  )
+  warning(warn)
+}
+
+## -----------------------------------------------------------------------------------
+## --------------- custom match.arg with call.=FALSE for stop()
+## -----------------------------------------------------------------------------------
+match.arg_custom <- function (arg, choices, several.ok = FALSE)
+{
+  if (missing(choices)) {
+    formal.args <- formals(sys.function(sysP <- sys.parent()))
+    choices <- eval(formal.args[[as.character(substitute(arg))]],
+                    envir = sys.frame(sysP))
+  }
+  if (is.null(arg))
+    return(choices[1L])
+  else if (!is.character(arg))
+    stop("'arg' must be NULL or a character vector")
+  if (!several.ok) {
+    if (identical(arg, choices))
+      return(arg[1L])
+    if (length(arg) > 1L)
+      stop("'arg' must be of length 1")
+  }
+  else if (length(arg) == 0L)
+    stop("'arg' must be of length >= 1")
+  i <- pmatch(arg, choices, nomatch = 0L, duplicates.ok = TRUE)
+  if (all(i == 0L))
+    stop(paste(match.call()$arg,gettextf("should be one of %s", paste(dQuote(choices),
+                                                                      collapse = ", "))), call.=FALSE, domain = NA)
+  i <- i[i > 0L]
+  if (!several.ok && length(i) > 1)
+    stop("there is more than one match in 'match.arg'")
+  choices[i]
+}
